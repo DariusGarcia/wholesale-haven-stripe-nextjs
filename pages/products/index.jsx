ec9018ca8cc52b18297d6d351408906f81bc00a9
Darius Garcia <dariusgarcia888@gmail.com>
2023-11-07 16:43:22 -0800

    add cart icon to bottom of products page

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,10 +1,19 @@
 import Stripe from 'stripe'
 import Card from '../../components/Card'
 import { useState } from 'react'
+import { useCart } from '../../context/CartContext'
+import { ShoppingBagIcon } from '@heroicons/react/24/outline'
+
+import ShoppingCartSlideOver from '../../components/ShoppingCart'
 
 const HomePage = ({ prices: { data = [], has_more } }) => {
   const [products, setProducts] = useState(data)
   const [hasMore, setHasMore] = useState(has_more)
+
+  const { items } = useCart()
+  const [open, setOpen] = useState(false)
+
+  const [cartSliderIsOpen, setCartSliderIsOpen] = useState(false)
 
   const lastProductId = products[products.length - 1]?.id
 
@@ -25,25 +34,54 @@ const HomePage = ({ prices: { data = [], has_more } }) => {
   }
 
   return (
-    <div className='bg-white'>
-      <div className='max-w-2xl mx-auto p-4 sm:p-8 lg:max-w-7xl'>
-        <h2 className='text-2xl font-bold text-gray-900'>Products</h2>
+    <>
+      {' '}
+      <div className='z-50 fixed bottom-10 right-5 '>
+        <div className=' bg-sky-200 hover:bg-sky-100 transition ease-in-out   items-start flex justify-center rounded-full'>
+          <ShoppingCartSlideOver
+            open={cartSliderIsOpen}
+            setCartSliderIsOpen={setCartSliderIsOpen}
+          />
 
-        <div className='mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8'>
-          {products.map(price => (
-            <Card key={price.id} price={price} />
-          ))}
-        </div>
-
-        <button
-          onClick={loadMore}
-          disabled={!hasMore}
-          className='mt-10 w-full bg-sky-100 rounded-md py-2 px-8 text-sm font-medium text-sky-900 hover:bg-sky-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-sky-100'
-        >
-          Load more
-        </button>
+          {!cartSliderIsOpen && (
+            <div className='flow-root p-2'>
+              <div
+                className='group p-2 flex items-center cursor-pointer'
+                onClick={() => setCartSliderIsOpen(open => !open)}
+              >
+                <ShoppingBagIcon
+                  className='flex-shrink-0 h-5 w-5 text-sky-700 group-hover:text-sky-800'
+                  aria-hidden='true'
+                />
+                <span className='ml-2 text-sm font-medium text-sky-700 group-hover:text-sky-800'>
+                  ( {items.length} )
+                </span>
+                <span className='sr-only'>items in cart, view bag</span>
+              </div>
+            </div>
+          )}
+        </div>{' '}
       </div>
-    </div>
+      <div className='relative bg-white'>
+        <div className='max-w-2xl mx-auto p-4 sm:p-8 lg:max-w-7xl'>
+          <h2 className='text-2xl font-bold text-gray-900'>Products</h2>
+
+          <div className='mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8'>
+            {products.map(price => (
+              <Card key={price.id} price={price} />
+            ))}
+          </div>
+
+          <button
+            onClick={loadMore}
+            disabled={!hasMore}
+            className='mt-10 w-full bg-sky-100 rounded-md py-2 px-8 text-sm font-medium text-sky-900 hover:bg-sky-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-sky-100'
+          >
+            Load more
+          </button>
+        </div>{' '}
+      </div>
+    </>
   )
 }
 

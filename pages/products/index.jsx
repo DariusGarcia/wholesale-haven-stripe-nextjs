da461346e8059cc67dc1e9e82f330b3e4038e58e
Darius Garcia <dariusgarcia888@gmail.com>
2023-11-07 21:52:50 -0800

    update products page

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -37,7 +37,7 @@ const HomePage = ({ prices: { data = [], has_more } }) => {
     <>
       {' '}
       <div className='z-50 fixed bottom-10 right-5 '>
-        <div className=' bg-sky-200 hover:bg-sky-100 transition ease-in-out   items-start flex justify-center rounded-full'>
+        <div className='bg-sky-200 hover:bg-sky-100 transition ease-in-out   items-start flex justify-center rounded-full'>
           <ShoppingCartSlideOver
             open={cartSliderIsOpen}
             setCartSliderIsOpen={setCartSliderIsOpen}
@@ -64,21 +64,24 @@ const HomePage = ({ prices: { data = [], has_more } }) => {
       </div>
       <div className='relative bg-white'>
         <div className='max-w-2xl mx-auto p-4 sm:p-8 lg:max-w-7xl'>
-          <h2 className='text-2xl font-bold text-gray-900'>All Products</h2>
+          <h2 className='pl-4 md:pl-0 mt-2 text-2xl font-bold text-gray-900'>
+            All Products
+          </h2>
 
-          <div className='mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8'>
+          <div className='md:mt-8 grid grid-cols-2 gap-y-12 m:gap-x-6 lg:grid-cols-4 xl:gap-x-8'>
             {products.map(price => (
               <Card key={price.id} price={price} />
             ))}
           </div>
-
-          <button
-            onClick={loadMore}
-            disabled={!hasMore}
-            className='mt-10 w-full bg-sky-100 rounded-md py-2 px-8 text-sm font-medium text-sky-900 hover:bg-sky-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-sky-100'
-          >
-            Load more
-          </button>
+          <div className='my-12'>
+            <button
+              onClick={loadMore}
+              disabled={!hasMore}
+              className='mt-10 w-full bg-sky-100 rounded-md py-2 px-8 text-sm font-medium text-sky-900 hover:bg-sky-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-sky-100'
+            >
+              Load more
+            </button>
+          </div>
         </div>{' '}
       </div>
     </>

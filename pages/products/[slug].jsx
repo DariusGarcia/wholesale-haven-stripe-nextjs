feb173269915afb917138e66e08414b209ed47fc
Darius Garcia <dariusgarcia888@gmail.com>
2023-11-13 13:20:44 -0800

    fix cart button

--- pages/products/[slug].jsx
+++ pages/products/[slug].jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState } from 'react'
 import { Disclosure, RadioGroup, Tab } from '@headlessui/react'
 import { StarIcon } from '@heroicons/react/20/solid'
@@ -146,7 +147,7 @@ export default function ProductOverview() {
               <div className='mt-10 flex'>
                 <button
                   type='submit'
-                  className='flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-sky-600 px-8 py-3 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full'
+                  className='flex w-full md:max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-sky-600 px-8 py-3 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full'
                 >
                   Add to cart
                 </button>

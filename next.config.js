ca8ee098ac75145f392e491460f09c61e6c9aaa3
Darius Garcia <dariusgarcia888@gmail.com>
2023-11-16 18:23:29 -0800

    add domain to next.config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['files.stripe.com', 'images.unsplash.com']
+    domains: ['files.stripe.com', 'images.unsplash.com', 'cdn.sanity.io/images']
   }
 }
 

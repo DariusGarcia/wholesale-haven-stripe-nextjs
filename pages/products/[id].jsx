69019f9e909c8118dd7f894aabce74cb5db1da86
Darius Garcia <dariusgarcia888@gmail.com>
2023-11-16 19:44:13 -0800

    add btn

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -93,6 +93,16 @@ export default function ProductOverview({ productDetails, matchedProduct }) {
                 <p>{matchedProduct?.product?.description}</p>
               </div>
             </div>
+            <form className='mt-6'>
+              <div className='mt-10 flex'>
+                <button
+                  type='submit'
+                  className='flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-sky-600 px-8 py-3 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 transition ease-in-out focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full'
+                >
+                  Add to bag
+                </button>
+              </div>
+            </form>
 
             <section aria-labelledby='details-heading' className='mt-12'>
               <h2 id='details-heading' className='sr-only'>
